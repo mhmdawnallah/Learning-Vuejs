c7e1e0dbb89535e3fc43ef851be7f0daef4d10cc
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-18 16:31:35 +0200

    Explore Event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,17 +3,24 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: ""
     };
   },
   methods: {
+    confirmName(){
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
     add(number) {
       this.counter += number;
     },
     reduce(number) {
       this.counter -= number;
-    },
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
     }
   }
 });
