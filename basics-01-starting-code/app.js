75ccb04e304c176a8551f9b4856f0a15f9ee23cd
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-18 15:20:26 +0200

    Learn more about methods in Vue js and integrate it with HTML

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: "Finish the course by 20 March 2023 and Learn Vue js!"
+            courseGoal: "Finish the course by 20 March 2023 and Learn Vue js!",
+            vueLink: "https://vuejs.org/"
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomnumber = Math.random();
+            if (randomnumber < 0.5) {
+                return "Learn Vue";
+            } else {
+                return "Learn Vue and Contribute!";
+            }
+        }
     }
 });
 
