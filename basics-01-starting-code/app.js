d2df689edd09eb3ddb0abb6857c70478f27dfa55
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-18 15:06:36 +0200

    Inteporalte data properties vue js into html

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: "Finish the course by 20 March 2023 and Learn Vue js!"
+        };
+    }
+});
+
+app.mount("#user-goal");
