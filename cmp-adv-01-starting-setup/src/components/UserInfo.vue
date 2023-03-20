174c663f3cae23c74ad51e65836684a5a332004c
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-20 20:13:03 +0200

    Add named slots to add multiple slots into the downstream component

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,11 +1,13 @@
 <template>
-  <section>
-    <header>
+  <base-card>
+    <template v-slot:header>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
-  </section>
+    </template>
+    <template v-slot:default>
+      <p>{{ infoText }}</p>
+    </template>
+  </base-card>
 </template>
 
 <script>
@@ -15,13 +17,6 @@ export default {
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
 
 section header {
   display: flex;
