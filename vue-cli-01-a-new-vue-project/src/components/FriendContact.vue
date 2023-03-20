9c4240d640a9b3b25e9824a3851bdb94520f8cac
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-20 04:54:48 +0200

    Add vue-cli-01-a-new-vue-project

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{  friend.name }}</h2>
+        <button @click="toggleDetails">{{ areDetailsVisible ? 'Hide Details' : 'Show Details' }}</button>
+        <ul v-if="areDetailsVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return {
+            areDetailsVisible: false,
+            friend: {
+                id: 'mohamed',
+                name: "Mohamed Awnallah",
+                phone: "0123 456 7121",
+                email: "mohamedawnallah@localhost.com"
+            },
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.areDetailsVisible = !this.areDetailsVisible;
+        }
+    },
+    compute: {}
+};
+</script>
