e63b8c9e69b828d07be30f1e4fdbcef1ec64fd1d
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-20 19:42:55 +0200

    Register local components for performance and readability and enhancement

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  components: {
+    TheHeader: TheHeader,
+    BadgeList: BadgeList,
+    UserInfo: UserInfo
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Mohamed Awnallah',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
