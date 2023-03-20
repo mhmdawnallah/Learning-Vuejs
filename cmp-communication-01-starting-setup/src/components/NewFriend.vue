5046a7630a8c555913d63b752cf7e594b1d59b5c
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-20 17:06:28 +0200

    Add New Friend form components and connect them together

---
+++ cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+<form @submit.prevent="submitData">
+    <div >
+        <label>Friend Id:</label><br>
+        <input type="text" v-model="friendId"><br>
+    </div>
+    <div>
+        <label>Friend Name:</label><br>
+        <input type="text"  v-model="friendName"><br>
+    </div>
+    <div>
+        <label>Phone Number:</label><br>
+        <input type="text"  v-model="phoneNumber"><br>
+    </div>
+    <div>
+        <label>Email Address:</label><br>
+        <input type="text" v-model="emailAddress"><br>
+    </div>
+    <button>Add Contact</button>
+</form>
+
+</template>
+
+<script>
+export default {
+    emits: ['add-contact'],
+    data() {
+        return {
+            friendId: '',
+            friendName: '',
+            phoneNumber: '',
+            emailAddress: ''
+        }
+    },
+    methods: {
+        submitData(){
+            this.$emit("add-contact",this.friendId, this.friendName, this.phoneNumber, this.emailAddress);
+        }
+    }
+}
+</script>
+
